cbeb356ac7bb7b4eb3d12d51b49483dff7675e04
Daniel Praciano <daniel.praciano@lsbd.ufc.br>
2017-12-16 10:50:22 -0300

    OpenGL started

---
+++ Scenario_CG/main/include/myglwidget.h
@@ -0,0 +1,49 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QGLWidget>
+#include <GL/glu.h>
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyGLWidget(QWidget *parent = 0);
+    ~MyGLWidget();
+signals:
+
+public slots:
+
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+public slots:
+    // slots for xyz-rotation slider
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+
+signals:
+    // signaling rotation from mouse movement
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
+
+private:
+    void draw();
+
+    int m_xRot;
+    int m_yRot;
+    int m_zRot;
+
+    QPoint m_lastPos;
+};
+
+#endif // GLWIDGET_H
